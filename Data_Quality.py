f956dffa4d7636cd43bec148bcdaedb0480d0e36
alezanper <fabenavideso@gmail.com>
2018-10-06 14:44:31 -0500

    null parts included

--- Data_Quality.py
+++ Data_Quality.py
@@ -8,37 +8,72 @@ Created on Sun Sep 01 13:00:15 2018
 import pandas as pd 
 import itertools
 
-encoding = 'iso-8859-1'
 
 class Rules:  
     
+    encoding = 'iso-8859-1'    
     data = pd.DataFrame()   # Data to work with
     counter = 0             # Size of dataFrame
     goods = False           # False for return bad registers
     delimiter = ','
+    parts = []
     
     """
     Loading file into dataframe
     """
     def __init__(self, filename, delimiter, goods):
-        self.data = pd.read_csv(filename,
-                       delimiter = delimiter,
-                       encoding = encoding)
-        
-        self.counter = (self.data).shape[0]
         self.goods = goods
         self.delimiter = delimiter
-        
+        self.parts = self.split(filename, 70)
+#        self.data = pd.read_csv(filename,
+#                       delimiter = delimiter,
+#                       encoding = encoding)        
+#       self.counter = (self.data).shape[0]
+
+    
+    """
+    """
+    def getDataFrame(self, filename):
+        data = pd.read_csv(filename,
+                       delimiter = self.delimiter,
+                       encoding = self.encoding)
+        return data
+    
+    """
+    Split
+    """
+    def split(self, filename, numLines):
+        parts = []
+        with open(filename, encoding = self.encoding) as f:
+            file = f.readlines()
+            size = len(file)
+            
+            for i in range(int(size/numLines)+1):
+                with open('part_' + str(i) + '_' + filename, "w", encoding = self.encoding) as csv:
+                    parts.append('part_' + str(i) + '_' + filename)
+                    csv.write(''.join(list(itertools.islice(file, 0, 1))))
+                    csv.write(''.join(list(itertools.islice(file, 1, numLines)))) if (i == 0) else csv.write(''.join(list(itertools.islice(file, numLines*i, numLines + numLines*i))))
+        return parts
         
     """
     Checking for null values
     """
+#    def checkNull(self, columnToAnalize):
+#        #Checking for null values
+#        return self.data[self.data[columnToAnalize].isna()
+#                         == (not self.goods)]
+        
     def checkNull(self, columnToAnalize):
-        #Checking for null values
-        return self.data[self.data[columnToAnalize].isna()
-                         == (not self.goods)]
+        print(self.parts)
+        print(len(self.parts))
+        
+        for i in range(len(self.parts)):
+            dataPart = self.getDataFrame(self.parts[i])            
+            self.data = self.data.append(dataPart[dataPart[columnToAnalize].isna()
+                         == (not self.goods)])
+        return self.data
     
-    
+
     """
     Checking for unicity
     """
@@ -66,7 +101,7 @@ class Rules:
     def checkListReference(self, listname, columnToAnalize):   
         listref = pd.read_csv(listname,
                        delimiter = self.delimiter,
-                       encoding = encoding)
+                       encoding = self.encoding)
         
         return self.data[self.data[columnToAnalize].
                          isin(list(listref['job']))
@@ -80,19 +115,7 @@ class Rules:
         return self.checkPattern(columnToAnalize, '[\w]+@[\w]+.com')
         
     
-    """
-    Split
-    """
-    def split(filename, numLines):
-        with open(filename, encoding = encoding) as f:
-            file = f.readlines()
-            size = len(file)
-            
-            for i in range(int(size/numLines)+1):
-                with open('part_' + str(i) + '_' + filename, "w", encoding = encoding) as csv:
-                    csv.write(''.join(list(itertools.islice(file, 0, 1))))
-                    csv.write(''.join(list(itertools.islice(file, 1, numLines)))) if (i == 0) else csv.write(''.join(list(itertools.islice(file, numLines*i, numLines + numLines*i))))
-                
+#print(Rules.split('','people.csv', 20))
 
         
         
--- Main.py
+++ Main.py
@@ -7,7 +7,6 @@ Created on Sun Sep 01 13:09:29 2018
 
 import Data_Quality as DQ
 
-dataWork = DQ.Rules('people.csv', ';', False)
 
 
 #print(dataWork.checkListReference('jobs.csv', 'job'))
@@ -16,5 +15,15 @@ dataWork = DQ.Rules('people.csv', ';', False)
 
 #print(dataWork.checkNull('email'))
 
-print(dataWork.checkUnity('email', 7))
+#print(dataWork.checkUnity('email', 7))
 
+
+
+
+
+# Start data quality rules 
+dataWork = DQ.Rules('people.csv', 
+                    ';', 
+                    False) # False for retrieve bad registers only
+
+print(dataWork.checkNull2('email'))
